e62a3e3deda11eeacead3f5f872275dc34c80b88
Szybet <53944559+Szybet@users.noreply.github.com>
2023-07-30 18:36:11 +0200

    further modules hell, its great

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git submodule update --init --recursive
+
+cd mupdf
+export PATH=$PATH:/home/build/inkbox/compiled-binaries/arm-kobo-linux-gnueabihf/bin/
+
+CHOST=arm CC=arm-kobo-linux-gnueabihf-gcc \
+AR=arm-kobo-linux-gnueabihf-ar \
+RANLIB=arm-kobo-linux-gnueabihf-ranlib \
+CXX=arm-kobo-linux-gnueabihf-g++ \
+LINK=arm-kobo-linux-gnueabihf-g++ \
+LD=arm-kobo-linux-gnueabihf-ld \
+ARCH=arm CROSS_COMPILE=arm-kobo-linux-gnueabihf- make -j 8 HAVE_X11=no HAVE_GLUT=no
